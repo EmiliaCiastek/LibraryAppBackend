82bee00eee4cf0d8d1b0fe65b98f6f9be77765c9
Emilia Ciastek <emilia.ciastek@farmaprom.pl>
2020-07-25 16:54:22 +0200

    Add sample data as sql script

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ spring.datasource.url=jdbc:postgresql://localhost:5432/library
 spring.datasource.username=postgres 
 spring.datasource.password=password 
 spring.jpa.hibernate.ddl-auto=update
+
+spring.jpa.hibernate.ddl-auto=create
+spring.jpa.properties.hibernate.hbm2ddl.import_files=data.sql
 ```
 
 ## To start server on localhost: 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+insert into authors (id, name, last_name, birth_date, death_date, website, genres, photo_url, description) values (1, 'Svetlana', 'Isakova', '', '', '', '', '', '');
+insert into authors (id, name, last_name, birth_date, death_date, website, genres, photo_url, description) values (2, 'John Ferguson', 'Smart', '', '', '', '', '', '');
+insert into authors (id, name, last_name, birth_date, death_date, website, genres, photo_url, description) values (3, 'Robert C.', 'Martin', '', '', '', '', '', '');
+insert into authors (id, name, last_name, birth_date, death_date, website, genres, photo_url, description) values (4, 'Tess', 'Gerritsen', '', '', '', '', '', '');
+insert into authors (id, name, last_name, birth_date, death_date, website, genres, photo_url, description) values (5, 'Robyn', 'Harding', '', '', '', '', '', '');
+insert into authors (id, name, last_name, birth_date, death_date, website, genres, photo_url, description) values (6, 'John Ronald Reuel', 'Tolkien', '03-01-1892', '02-09-1973', 'http://www.tolkienestate.com/', 'Fantasy, Fiction, Children''s', 'https://i.wpimg.pl/O/464x660/d.wpimg.pl/822554738--93831743/j-r-r-tolkien-tolkien.jpg', 'John Ronald Reuel Tolkien, CBE was an English writer, poet, WWI veteran, philologist, and university professor, best known as the author of the high fantasy classic works The Hobbit and The Lord of the Rings.');
+
+--insert into books (id, title, author_id, cover_url, description) values (1, 'Bdd in action', 2,  '', '');
+--insert into books (id, title, author_id, cover_url, description) values (2, 'Clean code', 3,  '', '');
+--insert into books (id, title, author_id, cover_url, description) values (3, 'Gravity', 4,  '', '');
+--insert into books (id, title, author_id, cover_url, description) values (4, 'Clean Coder', 3,  '', '');
+--insert into books (id, title, author_id, cover_url, description) values (5, 'The party', 5, 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1497173835l/35405445._SY475_.jpg', 'One invitation. A lifetime of regrets. Sweet sixteen. It’s an exciting coming of age, a milestone, and a rite of passage. Jeff and Kim Sanders plan on throwing a party for their daughter, Hannah—a sweet girl with good grades and nice friends. Rather than an extravagant, indulgent affair, they invite four girls over for pizza, cake, movies, and a sleepover. What could possibly go wrong?');
+--insert into books (id, title, author_id, cover_url, description) values (6, 'Kotlin in Action', 1, '', '');
+--
+--insert into book_rate (id, rate, book_id) values (1, 4.0, 5);
+--insert into book_rate (id, rate, book_id) values (2, 2.0, 5);
+--insert into book_rate (id, rate, book_id) values (3, 3.5, 5);
+--insert into book_rate (id, rate, book_id) values (4, 5.0, 5);
+--insert into book_rate (id, rate, book_id) values (5, 5.0, 5);
